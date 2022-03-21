5af355657f20fa03391e64eb68c9a122d6dc0246
Mirpribili <mirpribili@ya.ru>
2022-03-21 18:10:56 +0300

    @RequestParam__commit

--- src/main/java/com/mvc_spring/mwa/MyController.java
+++ src/main/java/com/mvc_spring/mwa/MyController.java
@@ -3,6 +3,7 @@ package com.mvc_spring.mwa;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,14 +24,28 @@ public class MyController {
 //
 //        return "show-emp-details-view";
 //    }
+
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr." + empName;
+//        model.addAttribute("nameAttribute", empName);
+//
+//        return "show-emp-details-view";
+//    }
+
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
-        empName = "Mr." + empName;
+    public String showEmpDetails(
+            @RequestParam("employeeName")String empName,
+            Model model)
+    {
+//        String empName = request.getParameter("employeeName");
+        empName = "Mr." + empName + " !";
         model.addAttribute("nameAttribute", empName);
 
         return "show-emp-details-view";
     }
-
 
 }
